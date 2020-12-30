405b0be8ae59d76fd6385d635de996f86a413eee
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2020-12-30 18:48:54 +0530

    initial commit

--- exceptions/src/com/codewithzia/ExceptionsDemo.java
+++ exceptions/src/com/codewithzia/ExceptionsDemo.java
@@ -1,10 +1,53 @@
 package com.codewithzia;
 
-public class ExceptionsDemo {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+// public class ExceptionsDemo {
+//     public static void show(){
+//         sayHello(null);
+//     }
+//     public static void sayHello(String name){
+//         System.out.println(name.toUpperCase());
+//     }
+// }
+
+
+// public class ExceptionsDemo {
+//     public static void show(){
+//         var reader = new FileReader("file.txt");//FileNotFoundException
+
+//     }
+// }
+
+
+// public class ExceptionsDemo {
+//     public static void show(){
+//         try {
+//             var reader = new FileReader("file.txt");
+//             System.out.println("File opened");
+//         } catch (FileNotFoundException ex) {
+//             //TODO: handle exception
+//             ex.printStackTrace();
+//         }
+//     }
+// }
+
+
+public class ExceptionsDemo{
     public static void show(){
-        sayHello(null);
-    }
-    public static void sayHello(String name){
-        System.out.println(name.toUpperCase());
+        try{
+            var reader = new FileReader("file.txt");
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
+        }catch (FileNotFoundException e) {
+            System.out.println("File does not exist");
+            e.printStackTrace();
+        }catch (IOException e | ParseException e) {
+            System.out.println("Could not read data");
+        }
     }
 }
