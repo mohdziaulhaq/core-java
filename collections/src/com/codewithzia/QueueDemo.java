4d33214c658fb79eef9b29618e0d39758a5a5830
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2021-01-20 21:23:21 +0530

    Queue added

---
+++ collections/src/com/codewithzia/QueueDemo.java
@@ -0,0 +1,18 @@
+package com.codewithzia;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void show(){
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+        // b -> a -> c
+        var front = queue.peek();
+        System.out.println(front);
+
+
+    }
+}
